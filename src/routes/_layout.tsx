e21ce3cf7f39bcc2eae0d016377629201305ee7d
serenity <98162476+NekoDrone@users.noreply.github.com>
2026-05-11 12:25:00 +0800

    refactor: explicitly redirect loopback

--- src/routes/_layout.tsx
+++ src/routes/_layout.tsx
@@ -7,6 +7,18 @@ export const Route = createFileRoute("/_layout")({
     head: () => ({
         meta: [{ title: "Gemstone" }],
     }),
+    beforeLoad: () => {
+        if (
+            import.meta.env.DEV &&
+            typeof window !== "undefined" &&
+            window.location.hostname === "localhost"
+        ) {
+            window.location.replace(
+                window.location.href.replace("localhost", "127.0.0.1"),
+            );
+            throw new Error("For OAuth purposes, do not use localhost. You must use the loopback IP address at 127.0.0.1. Redirecting now.")
+        }
+    },
 });
 
 function RouteComponent() {
